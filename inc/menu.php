a22e52fdb48724b8320747fba1f65690208dbb63
FaizDev007 <faizdev007@gmail.com>
2021-06-24 11:40:12 +0530

    update

---
+++ inc/menu.php
@@ -0,0 +1,10 @@
+<h1 id="colorlib-logo" class="mb-4 mb-md-5"><a href="index.html" style="background-image: url(images/bg_1.jpg);">Mostudio</a></h1>
+<nav id="colorlib-main-menu" role="navigation">
+	<ul>
+		<li class="colorlib-active"><a href="index.html">Home</a></li>
+		<li><a href="gallery.html">Projects</a></li>
+		<li><a href="about.html">About</a></li>
+		<li><a href="pricing.html">Pricing</a></li>
+		<li><a href="contact.html">Contact</a></li>
+	</ul>
+</nav>
