5495dd7724e1b71d2fc5d618e253626d7932148d
faizdev007 <faizdev007@gmail.com>
2021-07-16 20:32:59 +0530

    updatehome

--- js/faizdev.js
+++ js/faizdev.js
@@ -1,7 +1,7 @@
-function autoResizeDiv()
+`function autoResizeDiv()
 {
     document.getElementById('main').style.height = window.innerHeight +'px';
     document.getElementById('aboutme').style.height = window.innerHeight +'px';
 }
 window.onresize = autoResizeDiv;
-autoResizeDiv();
+autoResizeDiv();`
