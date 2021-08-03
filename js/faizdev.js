3f42b2f5d5451e2ecac272a34db51ccd0ece5e9e
faizdev007 <faizdev007@gmail.com>
2021-08-04 02:39:30 +0530

    update new

--- js/faizdev.js
+++ js/faizdev.js
@@ -21,3 +21,31 @@ const year=date.getFullYear();
 
 const value=document.getElementById('year').innerHTML=year;
 
+var clicks = 0;
+
+document.querySelector('#menutag').addEventListener('click',function(){
+    clicks += 1;
+
+  if(clicks % 2 == 1)
+    { 
+        document.querySelector('aside').style.flex="unset";
+        document.querySelector('.mainheader img').style.classList="hidden";
+        document.querySelector('.mainheader ul').style.visibility="hidden";
+        document.querySelector('footer').style.visibility="hidden";
+        document.querySelector('aside').style.width="40px";
+        document.querySelector('aside').classList="slid2";
+        document.querySelector('#menutag').style.transform="rotate(270deg)";
+        document.querySelector('#menutag').style.transition=".5s";
+    }
+  else if(clicks % 2 == 0)
+  {
+    document.querySelector('.mainheader img').style.visibility="visible";
+    document.querySelector('.mainheader ul').style.visibility="visible";
+    document.querySelector('footer').style.visibility="visible";
+    document.querySelector('aside').classList="newslid";
+    document.querySelector('footer').style.with="full";
+    document.querySelector('aside').style.flex="";
+    document.querySelector('#menutag').style.transform="rotate(180deg)";
+  }
+})
+
