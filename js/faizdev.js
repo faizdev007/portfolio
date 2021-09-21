c9d191bfcbcad970d82311e4e473495554b9eb1a
faizdev007 <faizdev007@gmail.com>
2021-09-22 01:15:13 +0530

    upate 22-09-2021

--- js/faizdev.js
+++ js/faizdev.js
@@ -28,24 +28,15 @@ document.querySelector('#menutag').addEventListener('click',function(){
 
   if(clicks % 2 == 1)
     { 
-        document.querySelector('aside').style.flex="unset";
-        document.querySelector('.mainheader img').style.classList="hidden";
-        document.querySelector('.mainheader ul').style.visibility="hidden";
-        document.querySelector('footer').style.visibility="hidden";
-        document.querySelector('aside').style.width="40px";
-        document.querySelector('aside').classList="slid2";
-        document.querySelector('#menutag').style.transform="rotate(270deg)";
-        document.querySelector('#menutag').style.transition=".5s";
+      document.querySelector('.mainheader').style.display="none";
+      document.querySelector('#menutag').style.transform="rotate(0deg)";
+      document.querySelector('#menutag').style.width="fit-content";
+      document.querySelector('#menutag').style.transition=".5s";
     }
   else if(clicks % 2 == 0)
   {
-    document.querySelector('.mainheader img').style.visibility="visible";
-    document.querySelector('.mainheader ul').style.visibility="visible";
-    document.querySelector('footer').style.visibility="visible";
-    document.querySelector('aside').classList="newslid";
-    document.querySelector('footer').style.with="full";
-    document.querySelector('aside').style.flex="";
-    document.querySelector('#menutag').style.transform="rotate(180deg)";
+    document.querySelector('.mainheader').style.display="block";
+    document.querySelector('#menutag').style.transform="rotate(90deg)";
   }
 })
 
